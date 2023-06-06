3e069d3f4b419ea5d53735e7cad34d61a787b53f
Connor Bradley <c.bradley.dev@gmail.com>
2023-06-06 11:44:17 +0100

    Switched to PBandJ setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,15 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+export default {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: true,
-      },
-    },
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
   },
   docs: {
     autodocs: "tag",
   },
 };
-export default config;
--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-import "../../styles/index.css";
-
-export { default as Button } from "./Button";
