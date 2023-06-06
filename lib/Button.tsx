f50a936bcafae4eaf4ebc82b34b13be96acf31e5
Connor Bradley <c.bradley.dev@gmail.com>
2023-06-06 12:25:42 +0100

    Added tailwind

--- lib/Button.tsx
+++ lib/Button.tsx
@@ -1,27 +1,19 @@
-import { useState } from "react";
-import style from "./Button.module.css";
+"use client";
 
-type ButtonProps = {
-  type: "primary" | "secondary";
-  size: "small" | "medium" | "large";
-};
-export function Button({ type, size }: ButtonProps) {
+import { useState } from "react";
+// import style from "./Button.module.css";
+
+// type ButtonProps = {
+//   type: "primary" | "secondary";
+//   size: "small" | "medium" | "large";
+// };
+export function Button() {
   let [count, setCount] = useState(0);
 
   return (
     <button
       onClick={() => setCount((count) => count + 1)}
-      className={
-        style.button +
-        " " +
-        (type === "secondary" ? style.secondary : style.primary) +
-        " " +
-        (size === "small"
-          ? style.small
-          : size === "medium"
-          ? style.medium
-          : style.large)
-      }
+      className="bg-red-500 text-white p-2 rounded-md"
     >
       Count is {count}
     </button>
