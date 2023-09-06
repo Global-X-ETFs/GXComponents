2996589f181ec719a872c9de44e135dabb4e68b4
Connor Bradley <c.bradley.dev@gmail.com>
2023-09-06 10:58:20 +0100

    Added tabsgroup components

--- components/TabGroup/TabGroup.tsx
+++ components/TabGroup/TabGroup.tsx
@@ -20,7 +20,7 @@ const TabsList = React.forwardRef<
     <TabsPrimitive.List
       ref={ref}
       className={cn(
-        "inline-flex h-9 items-center justify-center rounded-lg bg-neutral-400 p-1 text-black",
+        "inline-flex h-9 items-center justify-center rounded-lg bg-neutral-600 p-1 text-black",
         className,
       )}
       {...props}
