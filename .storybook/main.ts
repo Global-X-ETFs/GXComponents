ca4d91d2243f804846c9a8df22bb10174970afa3
Connor Bradley <c.bradley.dev@gmail.com>
2023-09-22 12:52:21 +0100

    Updated storybook and added couple design stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
 export default {
-  stories: ["../components/**/*.stories.@(ts|tsx)"],
+  stories: ["../components/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-vite",
