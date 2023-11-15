28cacd3a06f20c0424edcce25d05ef4b7e7e1267
Connor Bradley <c.bradley.dev@gmail.com>
2023-11-15 13:52:08 +0000

    Added Accordion

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,6 +62,20 @@ export default {
         lg: "992px",
         xl: "1200px",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
     fontFamily: {
       sans: ["Brown", "sans-serif"],
