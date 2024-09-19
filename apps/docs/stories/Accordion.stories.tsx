a030919c425809f86700ae2c62c9d15026ec8b4c
Connor Bradley <c.bradley.dev@gmail.com>
2024-09-19 13:09:25 +0100

    Improved Accordion Story

--- apps/docs/stories/Accordion.stories.tsx
+++ apps/docs/stories/Accordion.stories.tsx
@@ -19,43 +19,36 @@ type Story = StoryObj<typeof meta>;
 export default meta;
 
 export const ExampleAccordion: Story = {
-  args: {
-    data: [
-      {
-        trigger: "Is it accessible?",
-        content: "Yes. It adheres to the WAI-ARIA design pattern.",
-      },
-      {
-        trigger: "Is it styled?",
-        content:
-          "Yes. It comes with default styles that matches the other components' aesthetic.",
-      },
-      {
-        trigger: "Is it animated?",
-        content:
-          "Yes. Its animated by default, but you can disable it if you prefer.",
-      },
-    ],
-  },
-  render: function Render(args: {
-    data: { trigger: string; content: string }[];
-  }) {
-    const [_, updateArgs] = useArgs();
-
+  render: function Render() {
+    const data = [{
+      trigger: "Lorem ipsum dolor sit amet",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      trigger: "Ut enim ad minim veniam",
+      content:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      trigger: "Duis aute irure dolor in reprehenderit",
+      content:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    }];
+    
     return (
       <Accordion
         type="single"
         collapsible
         className="w-full bg-neutral-50"
-        onChange={(event) => {
-          updateArgs({ data: event });
-        }}
       >
-        {args.data.map(
+        {data.map(
           (item: { trigger: string; content: string }, index: Key) => (
             <AccordionItem value={`item-${index}`} key={index}>
-              <AccordionTrigger>{item.trigger}</AccordionTrigger>
-              <AccordionContent>{item.content}</AccordionContent>
+              <AccordionTrigger className="text-lg font-bold py-4 px-2">
+                {item.trigger}
+              </AccordionTrigger>
+              <AccordionContent className="font-proxima pl-1">{item.content}</AccordionContent>
             </AccordionItem>
           )
         )}
