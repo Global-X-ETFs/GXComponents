b1ec6fe3b5118e969c7fac0c44d746c701635a6f
Connor Bradley <c.bradley.dev@gmail.com>
2024-09-23 13:20:24 +0100

    Updated stories

--- apps/docs/stories/Accordion.stories.tsx
+++ apps/docs/stories/Accordion.stories.tsx
@@ -4,8 +4,10 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
+  Button,
 } from "@global-x-etfs/gx-components";
 import type { Key } from "react";
+import { useState } from "react";
 
 const meta: Meta = {
   title: "Components/Accordion",
@@ -17,40 +19,87 @@ type Story = StoryObj<typeof meta>;
 
 export default meta;
 
-export const ExampleAccordion: Story = {
+export const BasicAccordion: Story = {
   render: function Render() {
-    const data = [{
-      trigger: "Lorem ipsum dolor sit amet",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    },
-    {
-      trigger: "Ut enim ad minim veniam",
-      content:
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-    {
-      trigger: "Duis aute irure dolor in reprehenderit",
-      content:
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-    }];
-    
+    const data = [
+      {
+        trigger: "Lorem ipsum dolor sit amet",
+        content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      },
+      {
+        trigger: "Ut enim ad minim veniam",
+        content:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      },
+      {
+        trigger: "Duis aute irure dolor in reprehenderit",
+        content:
+          "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      },
+    ];
+
     return (
-      <Accordion
-        type="single"
-        collapsible
-        className="w-full bg-neutral-50"
-      >
-        {data.map(
-          (item: { trigger: string; content: string }, index: Key) => (
-            <AccordionItem value={`item-${index}`} key={index}>
-              <AccordionTrigger className="text-lg font-bold py-4 px-2">
-                {item.trigger}
-              </AccordionTrigger>
-              <AccordionContent className="font-proxima pl-1">{item.content}</AccordionContent>
-            </AccordionItem>
-          )
-        )}
+      <Accordion type="single" collapsible className="w-full bg-neutral-50">
+        {data.map((item: { trigger: string; content: string }, index: Key) => (
+          <AccordionItem value={`item-${index}`} key={index}>
+            <AccordionTrigger className="text-lg font-bold py-4 px-2">
+              {item.trigger}
+            </AccordionTrigger>
+            <AccordionContent className="font-proxima pl-1">
+              {item.content}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    );
+  },
+};
+
+export const FilterAccordion: Story = {
+  render: function Render() {
+    const [selectedFilter, setSelectedFilter] = useState({
+      theme: "All",
+      count: 28,
+    });
+
+    const etfData = [
+      { theme: "All", count: 28 },
+      { theme: "Technology", count: 12 },
+      { theme: "Healthcare", count: 8 },
+      { theme: "Technology", count: 5 },
+      { theme: "Healthcare", count: 3 },
+    ];
+
+    return (
+      <Accordion className="mb-4 px-2" collapsible type="single">
+        <AccordionItem value="_">
+          <div className="inline-flex items-baseline text-marine font-proxima text-md font-bold pl-1 gap-2" >
+            Filter:
+            <AccordionTrigger
+              chevronStyle="large"
+              className="font-bold text-xl font-proximatight ml-4 capitalize"
+            >
+              {selectedFilter.theme} ({selectedFilter.count})
+            </AccordionTrigger>
+          </div>
+          <AccordionContent>
+            <div className="gap-2 w-full flex flex-wrap">
+              {etfData.map((item) => (
+                <Button
+                  className="capitalize"
+                  key={item.theme}
+                  onClick={() => {
+                    setSelectedFilter(item);
+                  }}
+                  variant="badge-neutral-600-full"
+                >
+                  {item.theme} ({item.count})
+                </Button>
+              ))}
+            </div>
+          </AccordionContent>
+        </AccordionItem>
       </Accordion>
     );
   },
--- apps/docs/stories/FundDetailsCard.stories.tsx
+++ apps/docs/stories/FundDetailsCard.stories.tsx
@@ -6,6 +6,11 @@ import {
   FundDetailsCardTitle,
   FundDetailsCardSubtitle,
   FundDetailsCardFooter,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  InformationCardToolTip,
 } from "@global-x-etfs/gx-components";
 
 const meta: Meta = {
@@ -20,8 +25,12 @@ export default meta;
 
 export const ExampleFundDetailsCard: Story = {
   render() {
-    const title = "Fund Details";
-    const subtitle = "As of 12/31/2020";
+    const title = "ETF Characteristics";
+    const subtitle = "As of Sep 20 2024";
+    const years = [
+      { year: 2019, value: { price_to_earnings: 34.55, price_to_book_value: 7.33 } },
+      { year: 2020, value: { price_to_earnings: 30.22, price_to_book_value: 6.17 } },
+    ];
 
     return (
       <FundDetailsCard>
@@ -30,11 +39,71 @@ export const ExampleFundDetailsCard: Story = {
           <FundDetailsCardSubtitle>{subtitle}</FundDetailsCardSubtitle>
         </FundDetailsCardHeader>
         <FundDetailsCardContent>
-          <div className="w-full h-96 bg-neutral-200 my-4 align-middle">
-            <p className="text-3xl font-bold items-center h-full text-neutral-900">
-              Content
-            </p>
-          </div>
+          <Table>
+            <TableBody className="text-[15px] font-proximatight border-b">
+              <TableRow>
+                <TableCell variant="row-head">
+                  Return on Equity
+                  <InformationCardToolTip>
+                    Return on equity is a measure of how well a company used
+                    reinvested earnings to generate additional earnings. It is
+                    calculated as net income divided by average common equity
+                    over the period. Net income is calculated on a “pro forma”
+                    basis using consensus EPS results & estimates for the
+                    individual constituents of each index, multiplied by the
+                    float-adjusted shares in the index.
+                  </InformationCardToolTip>
+                </TableCell>
+                <TableCell>12.38%</TableCell>
+                <TableCell />
+              </TableRow>
+              <TableRow>
+                <TableCell variant="row-head">
+                  Weighted Avg. Market Cap
+                  <InformationCardToolTip>
+                    The weighted average market capitalization of the companies
+                    held by the Fund.
+                  </InformationCardToolTip>
+                </TableCell>
+                <TableCell>1,051,460 M</TableCell>
+                <TableCell />
+              </TableRow>
+              <TableRow>
+                <TableCell />
+                {years.map((year) => (
+                  <TableCell key={year.year} variant="row-head">
+                    {year.year}
+                  </TableCell>
+                ))}
+              </TableRow>
+              <TableRow>
+                <TableCell variant="row-head">
+                  Price-to-earnings
+                  <InformationCardToolTip>
+                  The price-to-earnings ratio is a fundamental measure used to determine if an investment is valued appropriately. Each holding’s P/E is the latest closing price divided by its net income. Net income is calculated on a “pro forma” basis using consensus EPS results/estimates for the individual constituents of each index, multiplied by the float-adjusted shares in the index.
+                  </InformationCardToolTip>
+                </TableCell>
+                {years.map((year) => (
+                  <TableCell key={year.year}>
+                    {year.value.price_to_earnings}
+                  </TableCell>
+                ))}
+              </TableRow>
+              <TableRow>
+                <TableCell variant="row-head">
+                  Price-to-book value
+                  <InformationCardToolTip>
+                  The price to book value ratio is a fundamental measure used to determine if an investment is valued appropriately. The book value of a company is a measure of how much the company’s assets are worth assuming the company’s debts are paid off. Book value is defined as common equity as reported by the index constituents according to GAAP. Forecasts reflect pro-forma calculations using most recently reported figures, plus earnings less dividends.
+                  </InformationCardToolTip>
+                </TableCell>
+                {years.map((year) => (
+                  <TableCell key={year.year}>
+                    {year.value.price_to_book_value}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableBody>
+          </Table>
         </FundDetailsCardContent>
         <FundDetailsCardFooter></FundDetailsCardFooter>
       </FundDetailsCard>
