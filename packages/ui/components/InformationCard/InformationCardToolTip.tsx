f1c012afedaf8ec6036f1d0e05ef6339fec59ab5
hulakantony <hulak.antony1992@gmail.com>
2025-01-24 21:52:10 +0200

    Add ability to click on tooltip trigger on touch screens

--- packages/ui/components/InformationCard/InformationCardToolTip.tsx
+++ packages/ui/components/InformationCard/InformationCardToolTip.tsx
@@ -5,40 +5,84 @@ import cn from "../Utils/cn";
 import { Tooltip, TooltipTrigger, TooltipContent } from "../Tooltip";
 import { TooltipProps, TooltipProvider } from "@radix-ui/react-tooltip";
 
+function useIsTouchDevice() {
+  try {
+    return matchMedia('(hover: hover)').matches || matchMedia('(pointer: coarse)').matches
+  } catch {
+    // Assume that if browser too old to support matchMedia it's likely not a touch device
+    return true
+  }
+}
+
 export interface InformationCardTooltipProps
   extends React.HTMLAttributes<HTMLDivElement>, TooltipProps {
     delayDuration?: number;
   }
 
+const InformationCardToolTipTrigger = ({ className, ...props }) => {
+  return (
+    <span
+      className={cn(
+        "bg-orange rounded-full cursor-pointer text-md text-white ml-2 font-proxima p-1 leading-4 w-4 h-4 inline-flex items-center justify-center",
+        className,
+      )}
+      {...props}
+    >
+      ?
+    </span>
+  );
+}
+
 const InformationCardToolTip = React.forwardRef<
   HTMLDivElement,
   InformationCardTooltipProps
 >(({ className, children, delayDuration = 150, ...props }, ref) => {
+  const innerRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useImperativeHandle(ref, () => innerRef.current!, []);
+
+  const isTouchDevice = useIsTouchDevice();
+  const [isOpen, setIsOpen] = React.useState(props.defaultOpen ?? false);
+
+  const toggleMobileOpen = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  }
+
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (innerRef?.current && !innerRef?.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
+
   return (
     <TooltipProvider>
-    <Tooltip delayDuration={delayDuration} {...props}>
-      <TooltipTrigger>
-        
-          <span
-            className={cn(
-              "bg-orange rounded-full cursor-pointer text-md text-white ml-2 font-proxima p-1 leading-4 w-4 h-4 inline-flex items-center justify-center",
-              className,
-            )}
-          >
-            ?
-          </span>
+      <Tooltip 
+        delayDuration={isTouchDevice ? delayDuration : 0}
+        {...(!isTouchDevice ? { open: isOpen } : {} )}
+        {...props} 
+      >
+        <TooltipTrigger onClick={!isTouchDevice ? toggleMobileOpen : () => {}}>
+          <InformationCardToolTipTrigger className={className} />
         </TooltipTrigger>
         <TooltipContent
           className={cn(
             "bg-white rounded-md shadow-lg p-4 max-w-80 font-normal text-lg text-darkblue font-proxima leading-5 border-neutral-400 border text-wrap",
             className,
           )}
-          ref={ref}
+          ref={innerRef}
         >
           {children}
         </TooltipContent>
       </Tooltip>
-      </TooltipProvider>
+    </TooltipProvider>
   );
 });
 
